3e20fecec1e09a19e662322efad4fc009775913d
Logan Raarup <logan@logan.dk>
2020-06-03 00:03:47 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="serverless-wsgi",
-    version="1.7.4",
+    version="1.7.5",
     author="Logan Raarup",
     author_email="logan@logan.dk",
     description="Amazon AWS API Gateway WSGI wrapper",
