db4b33ef24e99b2795415ad83ab25fb903247c80
Logan Raarup <logan@logan.dk>
2021-08-03 10:34:37 +0200

    Switch to GitHub Actions

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="serverless-wsgi",
     version="2.0.0",
+    python_requires=">3.6",
     author="Logan Raarup",
     author_email="logan@logan.dk",
     description="Amazon AWS API Gateway WSGI wrapper",
